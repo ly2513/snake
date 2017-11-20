1770cbf09382cad171a2141d95801b5e72229045
ly2513 <626375290@qq.com>
2017-11-20 18:00:38 +0800

    修改Arrayable、Jsonable的命名空间

---
+++ Support/Arrayable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Snake\Support;
+
+interface Arrayable
+{
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray();
+}
---
+++ Support/HigherOrderTapProxy.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * User: yongli
+ * Date: 17/11/20
+ * Time: 12:13
+ * Email: yong.li@szypwl.com
+ * Copyright: 深圳优品未来科技有限公司
+ */
+namespace Snake\Support;
+
+class HigherOrderTapProxy
+{
+    /**
+     * The target being tapped.
+     *
+     * @var mixed
+     */
+    public $target;
+
+    /**
+     * Create a new tap proxy instance.
+     *
+     * @param $target
+     */
+    public function __construct($target)
+    {
+        $this->target = $target;
+    }
+
+    /**
+     * Dynamically pass method calls to the target.
+     *
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        $this->target->{$method}(...$parameters);
+
+        return $this->target;
+    }
+}
